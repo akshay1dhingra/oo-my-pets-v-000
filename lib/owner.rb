03615988b4bee1c9dcf0b2fb7163ed3b75f95eb1
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-12-13 22:45:47 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -56,12 +56,12 @@ class Owner
   end
 
   def sell_pets
-    pets.each do |species, animals|
-      animals.each do |animal|
-        animal = "nervous"
-      end
-    end
-    animals.clear
-  end
+     pets.each do |species, animals|
+       animals.each do |animal|
+         animal.mood = "nervous"
+       end
+       animals.clear
+     end
+   end
 
 end
